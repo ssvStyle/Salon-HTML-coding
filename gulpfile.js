9b34e8bcad798b9e9f64c5a88467a1a74730566a
Serj <ssv.style@gmail.com>
2020-09-26 13:11:34 +0300

    Clean proj

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
+    sass = require('gulp-sass'),
+    rigger = require('gulp-rigger');
+
+gulp.task('html', function () {
+    return gulp.src('src/templates/**/*.html')
+        .pipe(rigger())
+        .pipe(gulp.dest('prod'));
+});
+
+gulp.task('style', function () {
+    return gulp.src('src/scss/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([autoprefixer]))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('prod/css'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('src/js/**/*.js')
+        .pipe(gulp.dest('prod/js'));
+});
+
+gulp.task('img', function () {
+    return gulp.src('src/img/**/*')
+        .pipe(gulp.dest('prod/img'));
+});
+
+gulp.task('build', gulp.series(['html', 'style', 'img', 'js']));
